fbbe74292862d86582d8b7d0c8df119d37bb7a89
wang <wang@kaitian.com>
2024-08-26 19:53:14 +0800

    first

---
+++ src/main/java/org/staff/staffsystem/StaffSystemApplication.java
@@ -0,0 +1,13 @@
+package org.staff.staffsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StaffSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StaffSystemApplication.class, args);
+    }
+
+}
